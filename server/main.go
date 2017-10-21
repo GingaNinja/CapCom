f8bf355a01ac0428fa178157eb6999574e7f935d
Dave Arkell <dave@lightdark.co.uk>
2017-10-22 00:33:39 +0100

    add getnexttransaction to get the first transaction in the list

--- server/main.go
+++ server/main.go
@@ -150,12 +150,87 @@ type Transaction struct {
 	Date        time.Time
 }
 
+type Value struct {
+	Amount float64 `json:",string"`
+}
+
+type Details struct {
+	Value       Value
+	Description string
+	Completed   time.Time
+}
+
+type ApiTransaction struct {
+	ID      string
+	Details Details
+}
+
+type shortTransaction struct {
+	ID string
+}
+
+type transactionList struct {
+	Transactions []shortTransaction
+}
+
+func getTransactionsForAccount(client *http.Client, bankID string, accountID string) ([]string, error) {
+	path := fmt.Sprintf("https://apisandbox.openbankproject.com/obp/v1.2.1/banks/%s/accounts/%s/owner/transactions", bankID, accountID)
+	fmt.Printf("Aboutn to call %s\n", path)
+	resp, err := client.Get(path)
+	if err != nil {
+		return nil, fmt.Errorf("issue getting request: %v", err)
+	}
+	defer resp.Body.Close()
+	byt, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, fmt.Errorf("error reading body: %v", err)
+	}
+
+	var transactions transactionList
+
+	if err := json.Unmarshal(byt, &transactions); err != nil {
+		return nil, err
+	}
+
+	ids := []string{}
+	for _, t := range transactions.Transactions {
+		ids = append(ids, t.ID)
+	}
+	return ids, nil
+}
+
 func apiGetNextTransactionHandler(w http.ResponseWriter, r *http.Request) {
+	if !isAuthd(r) {
+		http.Redirect(w, r, "/", http.StatusFound)
+		return
+	}
+	session, _ := store.Get(r, sessionName)
+	token := session.Values["token"].(string)
+	tokenSecret := session.Values["tokenSecret"].(string)
+	tokenObject := oauth1.NewToken(token, tokenSecret)
+	httpClient := config.Client(oauth1.NoContext, tokenObject)
+
+	transactions, err := getTransactionsForAccount(httpClient, "rbs", "20171020")
+	if err != nil {
+		panic(err)
+	}
+
+	path := fmt.Sprintf("https://apisandbox.openbankproject.com/obp/v1.2.1/banks/rbs/accounts/20171020/owner/transactions/%s/transaction", transactions[0])
+	resp, _ := httpClient.Get(path)
+	defer resp.Body.Close()
+	byt, _ := ioutil.ReadAll(resp.Body)
+
+	var apiTransaction ApiTransaction
+
+	if err := json.Unmarshal(byt, &apiTransaction); err != nil {
+		panic(err)
+	}
+
 	transaction := Transaction{
-		ID:          "1",
-		Amount:      12.99,
-		Description: "stuff",
-		Date:        time.Now(),
+		ID:          apiTransaction.ID,
+		Amount:      apiTransaction.Details.Value.Amount,
+		Description: apiTransaction.Details.Description,
+		Date:        apiTransaction.Details.Completed,
 	}
 
 	b, err := json.Marshal(transaction)
