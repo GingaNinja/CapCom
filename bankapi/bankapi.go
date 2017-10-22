e636c3176d3caa65ddc7582e1c72b425e7d6ab87
Dave Arkell <dave@lightdark.co.uk>
2017-10-22 09:45:42 +0100

    separate bankapi stuff into its own class

---
+++ bankapi/bankapi.go
@@ -0,0 +1,122 @@
+package bankapi
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"time"
+)
+
+// BankAPI allows communication with a bank
+type BankAPI struct {
+	client *http.Client
+}
+
+type value struct {
+	Amount float64 `json:",string"`
+}
+
+type details struct {
+	Value       value
+	Description string
+	Completed   time.Time
+}
+
+type transaction struct {
+	ID      string
+	Details details
+}
+
+type shortTransaction struct {
+	ID string
+}
+
+type transactionList struct {
+	Transactions []shortTransaction
+}
+
+type Transaction struct {
+	ID          string
+	Amount      float64
+	Description string
+	Date        time.Time
+}
+
+// NewBankAPI is the constructor for the BankAPI
+func NewBankAPI(client *http.Client) BankAPI {
+	return BankAPI{client: client}
+}
+
+func (b BankAPI) GetTransactionsForAccount(bankID string, accountID string) ([]string, error) {
+	path := fmt.Sprintf("https://apisandbox.openbankproject.com/obp/v1.2.1/banks/%s/accounts/%s/owner/transactions", bankID, accountID)
+	resp, err := b.client.Get(path)
+	if err != nil {
+		return nil, fmt.Errorf("issue getting request: %v", err)
+	}
+	defer resp.Body.Close()
+	byt, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, fmt.Errorf("error reading body: %v", err)
+	}
+
+	var transactions transactionList
+
+	if err := json.Unmarshal(byt, &transactions); err != nil {
+		return nil, err
+	}
+
+	ids := []string{}
+	for _, t := range transactions.Transactions {
+		ids = append(ids, t.ID)
+	}
+	return ids, nil
+}
+
+func (b BankAPI) GetTransactionFromID(transactionID string) (tran Transaction, err error) {
+	path := fmt.Sprintf("https://apisandbox.openbankproject.com/obp/v1.2.1/banks/rbs/accounts/20171020/owner/transactions/%s/transaction", transactionID)
+	resp, err := b.client.Get(path)
+	if err != nil {
+		return tran, fmt.Errorf("error requesting transaction information: %v", err)
+	}
+	defer resp.Body.Close()
+	byt, _ := ioutil.ReadAll(resp.Body)
+
+	var apiTransaction transaction
+
+	if err := json.Unmarshal(byt, &apiTransaction); err != nil {
+		return tran, fmt.Errorf("error parsing json: %v", err)
+	}
+
+	tran = Transaction{
+		ID:          apiTransaction.ID,
+		Amount:      apiTransaction.Details.Value.Amount,
+		Description: apiTransaction.Details.Description,
+		Date:        apiTransaction.Details.Completed,
+	}
+	return
+}
+
+func (b BankAPI) GetPrivateAccounts() (jsonBytes []byte, err error) {
+	path := "https://apisandbox.openbankproject.com/obp/v1.2.1/accounts/private"
+	resp, err := b.client.Get(path)
+	if err != nil {
+		return jsonBytes, fmt.Errorf("error getting accounts: %v", err)
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return jsonBytes, fmt.Errorf("error reading response body: %v", err)
+	}
+	var dat map[string]interface{}
+
+	if err := json.Unmarshal(body, &dat); err != nil {
+		return jsonBytes, fmt.Errorf("error parsing json: %v", err)
+	}
+	jsonBytes, err = json.MarshalIndent(dat, "", "  ")
+	if err != nil {
+		return
+	}
+
+	return
+}
